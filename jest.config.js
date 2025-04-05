106ba263a48990a25224ace82dee8ab34d966827
Sourish <mukherjee.sourish09@gmail.com>
2025-04-05 22:25:11 +0530

    Adding test cases

---
+++ jest.config.js
@@ -0,0 +1,4 @@
+export const preset = "ts-jest";
+export const testEnvironment = "node";
+export const roots = ["<rootDir>/src", "<rootDir>/test"];
+export const testPathIgnorePatterns = ["/node_modules/", "/src/test.ts"];
